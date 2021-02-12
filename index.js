1f240a9868238e6d7e194fa8e83978b3f9103b47
Gabo <ghernandezlohaus.78@gmail.com>
2021-02-12 01:21:12 -0300

    Conversion back to CommonJS Module for better compatibility.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import lookupName from "./lib/lookupName.js";
-import lookupUUID from "./lib/lookupUUID.js";
-export default { lookupName, lookupUUID };
+const lookupName = require("./lib/lookupName");
+const lookupUUID = require("./lib/lookupUUID");
+module.exports = { lookupName, lookupUUID };
